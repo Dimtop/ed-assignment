5eaea4fb43fd090fbfb70563da3ecf93e8381670
Dimtop <44418460+Dimtop@users.noreply.github.com>
2024-05-15 15:28:56 +0300

    [exercise-2] fix: fixes broken behaviour and exceptions in about page
    * fix: fixes wrong spelling when requesting the backend version in about page component
    
    * fix: fixes wrong property spelling (bdy instead of body) when accessing the response object of the request for the version of the frontend

--- be-candidates/frontEnd/src/app/about/about.component.ts
+++ be-candidates/frontEnd/src/app/about/about.component.ts
@@ -1,35 +1,38 @@
-import {Component, OnInit} from '@angular/core';
-import {AboutService} from "../services/about.service";
-import {tap} from "rxjs/operators";
+import { Component, OnInit } from '@angular/core';
+import { tap } from 'rxjs/operators';
+import { AboutService } from '../services/about.service';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
-
   backVersion: string;
   frontVersion: string;
 
-  constructor(private aboutService: AboutService) {
-  }
+  constructor(private aboutService: AboutService) {}
 
   ngOnInit(): void {
     this.populateMask();
   }
 
   private populateMask() {
-    this.aboutService.getVersion("backssssend").pipe(
-      tap((response: any) => {
-        this.backVersion = response.body.backend;
-      })
-    ).subscribe();
-    this.aboutService.getVersion("frontend").pipe(
-      tap((response: any) => {
-        this.frontVersion = response.bdy.frontend;
-      })
-    ).subscribe();
+    this.aboutService
+      .getVersion('backend')
+      .pipe(
+        tap((response: any) => {
+          this.backVersion = response.body.backend;
+        })
+      )
+      .subscribe();
+    this.aboutService
+      .getVersion('frontend')
+      .pipe(
+        tap((response: any) => {
+          this.frontVersion = response.body.frontend;
+        })
+      )
+      .subscribe();
   }
-
 }
