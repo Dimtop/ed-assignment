995117efbcf2aa8f9b2e4a37a62395a38f1fe4ab
Dimtop <44418460+Dimtop@users.noreply.github.com>
2024-05-16 13:26:50 +0300

    [exercise-4] feature: adds unit tests for UserController (#4)
    Complete the unit tests implementations for UserController

--- be-candidates/backEnd/src/test/java/com/dterz/BaseTests.java
+++ be-candidates/backEnd/src/test/java/com/dterz/BaseTests.java
@@ -1,11 +1,13 @@
 package com.dterz;
 
-import com.dterz.model.*;
-import com.dterz.repositories.AccountRepository;
-import com.dterz.repositories.InfoRepository;
-import com.dterz.repositories.TransactionsRepository;
-import com.dterz.repositories.UserRepository;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.List;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,13 +17,16 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.HashSet;
-import java.util.List;
-
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import com.dterz.model.Account;
+import com.dterz.model.Info;
+import com.dterz.model.Transaction;
+import com.dterz.model.TransanctionType;
+import com.dterz.model.User;
+import com.dterz.repositories.AccountRepository;
+import com.dterz.repositories.InfoRepository;
+import com.dterz.repositories.TransactionsRepository;
+import com.dterz.repositories.UserRepository;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -46,12 +51,15 @@ public class BaseTests {
     InfoRepository infoRepository;
 
     Account account = new Account(1L, "testAccount", new Date(), new HashSet<>(), new ArrayList<>());
-    User user = new User(1L, "spring", "", "spring", "spring", "", true, new ArrayList<>(), new HashSet<>(), new HashSet<>());
+    User user = new User(1L, "spring", "", "spring", "spring", "", true, new ArrayList<>(), new HashSet<>(),
+            new HashSet<>());
     List<Transaction> income = new ArrayList<>();
     List<Transaction> expences = new ArrayList<>();
     List<Account> accounts = new ArrayList<>();
-    Transaction t1 = new Transaction(1L, BigDecimal.valueOf(100), new Date(), "description", TransanctionType.INCOME, user, account);
-    Transaction t2 = new Transaction(2L, BigDecimal.valueOf(75), new Date(), "description", TransanctionType.EXPENCE, user, account);
+    Transaction t1 = new Transaction(1L, BigDecimal.valueOf(100), new Date(), "description", TransanctionType.INCOME,
+            user, account);
+    Transaction t2 = new Transaction(2L, BigDecimal.valueOf(75), new Date(), "description", TransanctionType.EXPENCE,
+            user, account);
     Info info = new Info(1L, "Backend", "1.0.0");
 
     @BeforeEach
